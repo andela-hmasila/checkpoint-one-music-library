02b44c5b7dabac187392ba0105c1ed41a5f97f34
andela-hmasila <hannah.masila@andela.com>
2016-08-17 00:15:44 +0300

    Adds the CLI commands and options.

--- lib/cli_and_music_library_controller.rb
+++ lib/cli_and_music_library_controller.rb
@@ -12,61 +12,106 @@ class MusicLibraryController
   end
 
   def call
-    puts "Kindly enter a command"
-    input = gets.chomp.downcase!
+    puts cli_start
+    input = ''
+
+    while input != 'exit'
+      input = gets.chomp
+
     if cli_options.include? input
       send(cli_options[input])
+    elsif input == 'help'
+      puts cli_commands
     else
-      self.undefined
+      #puts undefined
+      break if input == 'exit'
+
     end
   end
 
 
+  end
+  def cli_start
+    "
+    ========================
+          Music Library
+    ========================
+    "
+  end
+  def cli_commands
+    "Commands:
+    1. list songs # lists all songs
+    2. list genres # lists all genres
+    3. list genre # lists a particular genre
+    4. list artist # lists a particular artist
+    5. play song # play a particular song
+
+    exit # quit Ruby Muse
+    "
+
+
+  end
+
   def cli_options
     {
-      'list songs' => :list_songs,
+      'list songs' => :list_songs ,
       'list artists' => :list_artists,
       'list genres' => :list_genres,
       'play song' => :play_song,
       'list artist' => :list_artist,
-      'list genre' => :list_genre,
-      'exit' => :exit
+      'list genre' => :list_genre
+
     }
   end
   def list_songs
-    Song.all.each.with_index(1) do |song, idx|
-     puts "#{idx}. #{song}".colorize(:light_blue)
-  #  @music_importer.import
+    Song.all.each_with_index do
+      |song, idx|
+      puts "#{idx + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    end
+
   end
-end
   def list_artists
-    Artist.all
+    Artist.all.each do
+      |artist|
+      puts "#{artist.name}"
+    end
   end
   def list_genres
-    Artist.all
+    Genre.all.each do
+      |genre|
+      puts "#{genre.name}"
+    end
   end
+  def play_song
+    puts "Enter a song to play "
+    song_number = gets.chomp
+
+    song = Song.all[song_number.to_i-1]
+    puts "Playing #{song.artist.name} - #{song.name} - #{song.genre.name}"
+
+  end
+  def list_genre
+    print "Enter a genre to list "
+    genre = gets.chomp
+    Genre.all.each do
+      |genre|
+      puts "#{genre.name}"
+    end
+  end
+    def list_artist
+      print "Enter an artist to list "
+      artist = gets.chomp
+      Artist.find_by_name(artist) do
+        |artist|
+        puts "#{artist.name}"
+      end
+    end
+
   def undefined
-    puts "I did not understand you"
+    "I did not understand you"
   end
 
-  def cli_start
-    "
-    ========================
-          Ruby Muse
-    ========================
-    ".colorize(:cyan)
-    " Commands:
-    1.) list songs # lists all songs
-    2.) list artists # lists all artists
-    3.) list genres # lists all genres
-    4.) list genre # lists a particular genre
-    5.) list artist # lists a particular artist
-    6.) play song # play a particular song
 
-    exit # quit Ruby Muse
-    ".colorize(:green)
-    puts call
-  end
 
 
 

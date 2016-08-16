8928eb735166e6ff061a4b18425e11e263441702
andela-hmasila <hannah.masila@andela.com>
2016-08-16 10:22:04 +0300

    Adds the genre class and the basic functions of a genre. Contains the relationship between an artist and genres, songs and a genre.

---
+++ lib/genre.rb
@@ -0,0 +1,50 @@
+class Genre
+  extend Concerns::Findable
+
+  attr_accessor(:name)
+  attr_reader(:songs)
+  #@@all is a class variable that saves all instances of the Song class
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  #a custom constructor that instantiates an instance using .new and saves that instance
+  def self.create(name)
+    Genre.new(name).save
+  end
+
+  #saves the instance
+  def save
+    @@all << self
+    self
+  end
+
+  #returns all the saved instances
+  def self.all
+    @@all
+  end
+
+  #emptys @@all
+  def self.destroy_all
+    @@all = []
+  end
+  #def songs=(song)
+  #    add_song song
+  #end
+  def add_song(song)
+    @songs << song unless @songs.include? song
+    song.genre = self unless song.genre
+
+  end
+  def artists
+    genre_artists = []
+    @songs.each do |song|
+      genre_artists << song.artist unless genre_artists.include? song.artist
+    end
+    genre_artists
+
+  end
+end

33890ca3e7fb5ca9ebba6386d86a4555159afe80
andela-hmasila <hannah.masila@andela.com>
2016-08-16 10:26:27 +0300

    Adds the CLI and Music library controller. Contains the call method which starts the CLI. Contains different options a user can choose to run the proram.

---
+++ lib/cli_and_music_library_controller.rb
@@ -0,0 +1,90 @@
+class MusicLibraryController
+
+  attr_reader(:path)
+  def initialize (path = "./db/mp3s")
+    self.path = path if path
+  end
+
+  def path=(file_path)
+    @path = file_path
+    @music_importer = MusicImporter.new(file_path)
+    @music_importer.import
+  end
+
+  def call
+    puts "Kindly enter a command"
+    input = gets.chomp.downcase!
+    if cli_options.include? input
+      send(cli_options[input])
+    else
+      self.undefined
+    end
+  end
+
+
+  def cli_options
+    {
+      'list songs' => :list_songs,
+      'list artists' => :list_artists,
+      'list genres' => :list_genres,
+      'play song' => :play_song,
+      'list artist' => :list_artist,
+      'list genre' => :list_genre,
+      'exit' => :exit
+    }
+  end
+  def list_songs
+    Song.all.each.with_index(1) do |song, idx|
+     puts "#{idx}. #{song}".colorize(:light_blue)
+  #  @music_importer.import
+  end
+end
+  def list_artists
+    Artist.all
+  end
+  def list_genres
+    Artist.all
+  end
+  def undefined
+    puts "I did not understand you"
+  end
+
+  def cli_start
+    "
+    ========================
+          Ruby Muse
+    ========================
+    ".colorize(:cyan)
+    " Commands:
+    1.) list songs # lists all songs
+    2.) list artists # lists all artists
+    3.) list genres # lists all genres
+    4.) list genre # lists a particular genre
+    5.) list artist # lists a particular artist
+    6.) play song # play a particular song
+
+    exit # quit Ruby Muse
+    ".colorize(:green)
+    puts call
+  end
+
+
+
+
+
+
+
+=begin
+    #puts "Welcome to the Music Library".center
+    puts "Enter 'list songs' to list all songs\n
+    Enter 'list artists' to list all artists\n
+    Enter 'list artists' to list all artists\n
+    Enter 'list genres' to list all genres\n
+    Enter 'exit' to exit"
+
+
+=end
+
+
+
+end

32385054f305ed20c3029f1a7e8e9db4821e808f
andela-hmasila <hannah.masila@andela.com>
2016-08-17 12:05:20 +0300

    Adds the song class that contains song names and relationship between songs and genres - and artists and songs

--- lib/song.rb
+++ lib/song.rb
@@ -1,34 +1,36 @@
+# Song class that initializes a song and its properties
 class Song
   extend Concerns::Findable
 
   attr_accessor(:name)
   attr_reader(:artist, :genre)
-  #@@all is a class variable that saves all instances of the Song class
+  # @@all is a class variable that saves all instances of the Song class
   @@all = []
 
-	def initialize(name, artist = nil, genre = nil)
-	  @name = name
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
     self.genre = genre if genre
     self.artist = artist if artist
   end
 
-  #a custom constructor that instantiates an instance using .new and saves that instance
+  # a custom constructor that instantiates an instance
+  # using .new and saves that instance
   def self.create(name, artist = nil, genre = nil)
     Song.new(name, artist, genre).save
   end
 
-  #saves the instance
+  # saves the instance
   def save
     @@all << self
     self
   end
 
-  #returns all the saved instances
+  # returns all the saved instances
   def self.all
     @@all
   end
 
-  #emptys @@all
+  # emptys @@all
   def self.destroy_all
     @@all = []
   end
@@ -46,20 +48,14 @@ class Song
   def self.new_from_filename(filename)
     song_instance = filename[0...-4].split(' - ')
     Song.new(song_instance[1],
-    Artist.find_or_create_by_name(song_instance[0]),
-    Genre.find_or_create_by_name(song_instance[2]))
-
+             Artist.find_or_create_by_name(song_instance[0]),
+             Genre.find_or_create_by_name(song_instance[2]))
   end
 
   def self.create_from_filename(filename)
     song_instance = filename[0...-4].split(' - ')
     Song.new(song_instance[1],
-    Artist.find_or_create_by_name(song_instance[0]),
-    Genre.find_or_create_by_name(song_instance[2])).save
-
-
+             Artist.find_or_create_by_name(song_instance[0]),
+             Genre.find_or_create_by_name(song_instance[2])).save
   end
-
-
-
 end

dd526d8dde608fef1ea2efdd3fba69b03490c197
andela-hmasila <hannah.masila@andela.com>
2016-08-17 12:04:21 +0300

    Adds the artist class that contains artist names and relationship between artists and genres - and artists and songs

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,41 +1,40 @@
+# Artist class that initializes an artist and its properties
 class Artist
-
   extend Concerns::Findable
 
   attr_accessor(:name)
   attr_reader(:songs)
-  #@@all is a class variable that saves all instances of the Song class
+  # @@all is a class variable that saves all instances of the Song class
   @@all = []
 
-	def initialize(name)
-	  @name = name
+  def initialize(name)
+    @name = name
     @songs = []
   end
 
-  #a custom constructor that instantiates an instance using .new and saves that instance
+  # a custom constructor that instantiates an instance
+  # using .new and saves that instance
   def self.create(name)
     Artist.new(name).save
   end
 
-  #saves the instance
+  # saves the instance
   def save
     @@all << self
     self
   end
 
-  #returns all the saved instances
+  # returns all the saved instances
   def self.all
     @@all
   end
 
-  #emptys @@all
+  # emptys @@all
   def self.destroy_all
     @@all = []
+  end
 
-end
-#def songs=(song)
-#  add_song song
-#end
+  # adding a song to an artist
   def add_song(song)
     @songs << song unless @songs.include? song
     song.artist = self unless song.artist
@@ -48,5 +47,4 @@ end
     end
     song_genres
   end
-
 end

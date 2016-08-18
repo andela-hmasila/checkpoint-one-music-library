c9e0a1782e3cbdd22dcc868cae3c807237d1514e
andela-hmasila <hannah.masila@andela.com>
2016-08-18 17:12:13 +0300

    Adds the genre class

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,10 +1,8 @@
-# Genre class that initializes a genre and its properties
 class Genre
   extend Concerns::Findable
 
   attr_accessor(:name)
   attr_reader(:songs)
-  # @@all is a class variable that saves all instances of the Song class
   @@all = []
 
   def initialize(name)
@@ -12,23 +10,19 @@ class Genre
     @songs = []
   end
 
-  # a custom constructor that instantiates a genre instance and saves it
   def self.create(name)
     Genre.new(name).save
   end
 
-  # saves the instance
   def save
     @@all << self
     self
   end
 
-  # returns all the saved instances
   def self.all
     @@all
   end
 
-  # emptys @@all
   def self.destroy_all
     @@all = []
   end

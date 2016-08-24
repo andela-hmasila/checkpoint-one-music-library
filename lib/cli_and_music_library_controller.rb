6b2857777cca376a21b4b2a2a068c71de7fe547b
andela-hmasila <hannah.masila@andela.com>
2016-08-24 17:38:57 +0300

    Separate views from controllers

--- lib/cli_and_music_library_controller.rb
+++ lib/cli_and_music_library_controller.rb
@@ -1,4 +1,3 @@
-require 'colorize'
 class MusicLibraryController
   attr_accessor :view
 
@@ -8,7 +7,7 @@ class MusicLibraryController
   end
 
   def call
-    puts view.cli_start
+    view.cli_start
     check_input
   end
 
@@ -20,14 +19,14 @@ class MusicLibraryController
       'play song' => :play_song,
       'list artist' => :list_artist,
       'list genre' => :list_genre,
-      'home' => :cli_commands
+      'help' => :cli_commands
     }
   end
 
   def check_input
     loop do
       view.prompt
-      user_input = gets.chomp
+      user_input = gets.chomp.strip
       break if user_input == 'exit'
       if cli_options.include? user_input
         send(cli_options[user_input])
@@ -38,62 +37,30 @@ class MusicLibraryController
   end
 
   def list_songs
-    Song.all.each_with_index do |song, index|
-       puts "#{index + 1}. #{song.artist.name} - #{song.name}" \
-       " - #{song.genre.name}".green
-    end
+    view.list_songs
   end
 
   def list_artists
-    Artist.all.each { |artist| puts artist.name.to_s }
+    view.list_artists
   end
 
   def list_genres
-    Genre.all.each { |genre| puts genre.name.to_s }
-  end
-
-  def play_song
-    song = check_for_integer
-    if song
-      puts "Playing #{song.artist.name} - #{song.name} - #{song.genre.name}"
-    else
-      puts "The song number could not be found"
-    end
-  end
-
-  def list_genre
-    print 'Enter a genre to list '
-    genre = gets.chomp
-    genres = Genre.find_by_name(genre)
-    if genres
-      genres.songs.each do |song|
-        puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"
-      end
-    else
-      puts "Genre not found"
-    end
+    view.list_genres
   end
 
   def list_artist
-    print 'Enter an artist to list '
-    artist = Artist.find_by_name(gets.chomp)
-    if artist
-      artist.songs.each do |song|
-        puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"
-      end
-    else
-      puts "Artist not found"
-    end
+    view.songs_by_artist
   end
 
-  def check_for_integer
-    print "Enter a number to play "
-    begin
-      song_number = Integer(gets.chomp)
-    rescue
-      puts "Kindly enter an integer"
-      retry
-    end
-    Song.all[song_number.to_i - 1]
+  def list_genre
+    view.songs_by_genre
+  end
+
+  def play_song
+    view.play_song
+  end
+
+  def cli_commands
+    view.cli_commands
   end
 end
--- lib/music_library_view.rb
+++ lib/music_library_view.rb
@@ -1,7 +1,7 @@
 require 'colorize'
 class MusicLibraryView
   def cli_start
-    "
+    puts "
     =*=*=*=*=*=*=*=*=*=*=*=*=
           Music Library
     =*=*=*=*=*=*=*=*=*=*=*=*=
@@ -9,7 +9,7 @@ class MusicLibraryView
   end
 
   def cli_commands
-    "Commands:
+    puts "Commands:
     1. list songs\t #lists all songs
     2. list genres\t #lists all genres
     3. list genre\t #lists a particular genre
@@ -29,8 +29,65 @@ class MusicLibraryView
     cli_commands
   end
 
-  def undefined
-    puts "Command not found!!".red
-    cli_commands
+  def list_songs
+    Song.all.each_with_index do |song, index|
+       puts "#{index + 1}. #{song.artist.name} - #{song.name}" \
+       " - #{song.genre.name}".light_blue
+    end
+  end
+
+  def list_artists
+    Artist.all.each { |artist| puts artist.name.to_s.light_blue }
+  end
+
+  def list_genres
+    Genre.all.each { |genre| puts genre.name.to_s.light_blue }
+  end
+
+  def play_song
+    song = check_for_integer
+    if song
+      puts "Playing #{song.artist.name} - #{song.name}" \
+      " - #{song.genre.name}".light_blue
+    else
+      puts "The song number could not be found".red
+    end
+  end
+
+  def songs_by_genre
+    print "Enter a genre to list ".green
+    genre = Genre.find_by_name(gets.chomp.strip)
+    if genre
+      genre.songs.each do |song|
+        puts "#{song.artist.name} - #{song.name}" \
+        " - #{song.genre.name}".light_blue
+      end
+    else
+      puts "Genre not found".red
+    end
+  end
+
+  def songs_by_artist
+    print "Enter an artist to list ".green
+    artist = Artist.find_by_name(gets.chomp.strip)
+    if artist
+      artist.songs.each do |song|
+        puts "#{song.artist.name} - #{song.name}" \
+        " - #{song.genre.name}".light_blue
+      end
+    else
+      puts "Artist not found".red
+    end
+  end
+
+  def check_for_integer
+    print "Enter a number to play ".green
+    begin
+      song_number = Integer(gets.chomp.strip)
+    rescue
+      puts "Kindly enter an integer".red
+      retry
+    end
+    Song.all[song_number.to_i - 1]
   end
 end
